18071ad7b2b1654359046bbf4160be94c70488d5
fredi <arro.fredi@gmail.com>
2019-10-13 16:33:30 +0300

    Added the function to add courses to the table

--- res/js/script.js
+++ res/js/script.js
@@ -22,5 +22,21 @@ $(function () {
         $('#add-course').toggle();
     })
 
+    // Save course fields and add to table
+    $('#save-course').click(function () {
+        let title = $('#title').val();
+        let semester = $('#semester').val();
+        let grade = $('#grade').val();
+        // got some help from here: https://stackoverflow.com/questions/1149958/jquery-count-number-of-rows-in-a-table
+        let number = $('#courses > tbody tr').length + 1;
+        $('#courses > tbody:last-child')
+            .append($('<tr>'))
+            // adding variable to string, got help from: https://stackoverflow.com/questions/3304014/how-to-interpolate-variables-in-strings-in-javascript-without-concatenation/50337497
+                .append($(`<td>${number}</td>`))
+                .append($(`<td>${title}</td>`))
+                .append($(`<td>${semester}</td>`))
+                .append($(`<td>${grade}</td>`));
+    })
+
 });
 
