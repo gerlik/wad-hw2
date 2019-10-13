418cb2c3c0aa0a599ae475d532b6db90096bc01d
fredi <arro.fredi@gmail.com>
2019-10-13 17:49:59 +0300

    Added updating GPA

--- res/js/script.js
+++ res/js/script.js
@@ -1,10 +1,56 @@
 $(function () {
 
-
     // js function to reset add course fields
     function reset_fields(){
         $('.input').val('');
         $('#add-course').toggle();
+    }
+
+    // js function to calculate gpa
+    function gpa(grades) {
+        var total = 0;
+        for (var i = 0; i < grades.length; i++) {
+            let grade = grades[i];
+            if (grade > 90) {
+                total += 4;
+            }
+            else {
+                if (grade > 80) {
+                    total += 3;
+                }
+                else {
+                    if (grade > 70) {
+                        total += 2;
+                    }
+                    else {
+                        if (grade > 60) {
+                            total += 1;
+                        }
+                        else {
+                            if (grade > 50) {
+                                total += 0.5;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return (total / i).toFixed(2)
+    }
+
+    // js function to update gpa
+    function update_gpa(gpa) {
+        $('#gpa').children().text(gpa)
+    }
+
+
+    // js function to get all the grades
+    function get_grades() {
+        let grades = [];
+        $('#courses td:nth-of-type(4)').each( function () {
+            grades.push($(this).html());
+        });
+        return grades
     }
 
     // Tab switch
@@ -36,16 +82,16 @@ $(function () {
         // got some help from here: https://stackoverflow.com/questions/1149958/jquery-count-number-of-rows-in-a-table
         let number = $('#courses > tbody tr').length + 1;
         $('#courses > tbody:last-child')
-            .append($('<tr>'))
+            .append($('<tr>')
             // adding variable to string, got help from: https://stackoverflow.com/questions/3304014/how-to-interpolate-variables-in-strings-in-javascript-without-concatenation/50337497
                 .append(`<td>${number}</td>`)
                 .append(`<td>${title}</td>`)
                 .append(`<td>${semester}</td>`)
-                .append(`<td>${grade}</td>`);
-        reset_fields()
+                .append(`<td>${grade}</td>`)
+            );
+        reset_fields();
+        update_gpa(gpa(get_grades()));
     });
-
-    // GPA calculations
 
 });
 
